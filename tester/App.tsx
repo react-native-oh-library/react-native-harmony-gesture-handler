e8daba03b3066b09b9745694ebc732202fdd9f6d
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-07-31 18:35:20 +0200

    add test case

--- tester/App.tsx
+++ tester/App.tsx
@@ -1,15 +1,40 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
-// import {Tester} from '@rnoh/testerino';
+import React, {useState} from 'react';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import {TestCase, TestSuite, Tester} from '@rnoh/testerino';
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
 
 function App({}): JSX.Element {
   return (
-    <ScrollView style={styles.container}>
-      {/* <Tester>
-        
-      </Tester> */}
-    </ScrollView>
+    <GestureHandlerRootView>
+      <ScrollView style={styles.container}>
+        <Tester>
+          <TestSuite name="react-native-gesture-handler">
+            <TestCase itShould="toggle color on tap">
+              <TapExample />
+            </TestCase>
+          </TestSuite>
+        </Tester>
+      </ScrollView>
+    </GestureHandlerRootView>
+  );
+}
+
+function TapExample() {
+  const [backgroundColor, setBackgroundColor] = useState('red');
+
+  const tap = Gesture.Tap().onStart(() => {
+    setBackgroundColor(prev => (prev === 'red' ? 'green' : 'red'));
+  });
+
+  return (
+    <GestureDetector gesture={tap}>
+      <View style={{width: 100, height: 32, backgroundColor}} />
+    </GestureDetector>
   );
 }
 
