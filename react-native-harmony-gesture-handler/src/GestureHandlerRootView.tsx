e8aa869902588bf50fc5f28bb58a426776f26fb4
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-08-01 17:59:43 +0200

    _add GestureHandlerRootView

---
+++ react-native-harmony-gesture-handler/src/GestureHandlerRootView.tsx
@@ -0,0 +1,34 @@
+import {ReactNativeViewAttributes, registerViewConfig} from "react-native-harmony"
+import * as React from 'react';
+import { PropsWithChildren } from 'react';
+import { ViewProps } from 'react-native';
+import { maybeInitializeFabric } from 'react-native-gesture-handler/src/init';
+import GestureHandlerRootViewContext from 'react-native-gesture-handler/src/GestureHandlerRootViewContext';
+
+
+const GestureHandlerRootViewNativeComponent = registerViewConfig('RNGestureHandlerRootView', () => ({
+  uiViewClassName: 'RNGestureHandlerRootView',
+  bubblingEventTypes: {},
+  directEventTypes: {},
+  validAttributes: {
+    ...ReactNativeViewAttributes.UIView,
+  },
+}));
+
+export interface GestureHandlerRootViewProps
+  extends PropsWithChildren<ViewProps> {}
+
+export default function GestureHandlerRootView(
+  props: GestureHandlerRootViewProps
+) {
+  // try initialize fabric on the first render, at this point we can
+  // reliably check if fabric is enabled (the function contains a flag
+  // to make sure it's called only once)
+  maybeInitializeFabric();
+
+  return (
+    <GestureHandlerRootViewContext.Provider value>
+      <GestureHandlerRootViewNativeComponent {...props} />
+    </GestureHandlerRootViewContext.Provider>
+  );
+}
