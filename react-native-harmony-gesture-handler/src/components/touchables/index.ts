4f8b504151871fe8c0d0dac8de386fa6d79c447d
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-08-23 13:36:17 +0200

    support TouchableWithoutFeedback
    Signed-off-by: chenyihao9 <286884497@qq.com>

--- react-native-harmony-gesture-handler/src/components/touchables/index.ts
+++ react-native-harmony-gesture-handler/src/components/touchables/index.ts
@@ -2,6 +2,6 @@
 export type { TouchableOpacityProps } from './TouchableOpacity';
 // export type { TouchableWithoutFeedbackProps } from './TouchableWithoutFeedback';
 // export { default as TouchableNativeFeedback } from './TouchableNativeFeedback';
-// export { default as TouchableWithoutFeedback } from './TouchableWithoutFeedback';
+export { default as TouchableWithoutFeedback } from './TouchableWithoutFeedback';
 export { default as TouchableOpacity } from './TouchableOpacity';
 // export { default as TouchableHighlight } from './TouchableHighlight';
