d60a3b888f75c8e61f7ed9a1dfec27a94129b935
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2023-09-12 18:56:54 +0200

    _native animated events

--- tester/harmony/react_native_modules/gesture_handler/src/main/ets/EventDispatcher.ts
+++ tester/harmony/react_native_modules/gesture_handler/src/main/ets/EventDispatcher.ts
@@ -2,7 +2,12 @@ import { RNInstanceManager } from "rnoh/ts"
 import { GestureStateChangeEvent, GestureUpdateEvent, GestureTouchEvent } from "./OutgoingEvent"
 import { RNGHLogger } from './RNGHLogger'
 
-export class EventDispatcher {
+export interface EventDispatcher {
+  onGestureHandlerStateChange(event: GestureStateChangeEvent): void
+  onGestureHandlerEvent(event: GestureStateChangeEvent | GestureUpdateEvent | GestureTouchEvent): void
+}
+
+export class JSEventDispatcher implements EventDispatcher {
   constructor(private rnInstanceManager: RNInstanceManager, private logger: RNGHLogger) {
   }
 
@@ -14,5 +19,21 @@ export class EventDispatcher {
   public onGestureHandlerEvent(event: GestureStateChangeEvent | GestureUpdateEvent | GestureTouchEvent) {
     this.logger.info(`onGestureHandlerEvent: ${JSON.stringify(event)}`)
     this.rnInstanceManager.emitDeviceEvent("onGestureHandlerEvent", event)
+  }
+}
+
+export class AnimatedEventDispatcher implements EventDispatcher {
+  constructor(private rnInstanceManager: RNInstanceManager, private logger: RNGHLogger, private viewTag: number) {
+  }
+
+  public onGestureHandlerStateChange(event: GestureStateChangeEvent) {
+    this.logger.info(`onGestureHandlerStateChange: ${JSON.stringify(event)}`)
+    this.rnInstanceManager.emitDeviceEvent("onGestureHandlerStateChange", event)
+  }
+
+  public onGestureHandlerEvent(event: GestureStateChangeEvent | GestureUpdateEvent | GestureTouchEvent) {
+    // TODO: update event?
+    this.logger.info(`onGestureHandlerEvent: ${JSON.stringify(event)}`)
+    this.rnInstanceManager.emitComponentEvent(this.viewTag, "onGestureHandlerEvent", event)
   }
 }
