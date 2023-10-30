ac9014301c2fe341596b96f56c06d2439935e655
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-10-30 17:57:03 +0100

    enable debug logs

--- tester/harmony/react_native_modules/gesture_handler/src/main/ets/RNGestureHandlerModule.ts
+++ tester/harmony/react_native_modules/gesture_handler/src/main/ets/RNGestureHandlerModule.ts
@@ -23,7 +23,7 @@ export class RNGestureHandlerModule extends TurboModule {
 
   constructor(ctx: TurboModuleContext) {
     super(ctx)
-    const debug = false
+    const debug = true // TODO: set me to false before releasing
     this.logger = debug ? new StandardRNGHLogger(ctx.logger, "RNGH") : new FakeRNGHLogger()
   }
 
