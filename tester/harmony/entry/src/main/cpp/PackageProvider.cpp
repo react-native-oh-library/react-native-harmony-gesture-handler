a0b582f3bede4e1ef74ccb16c31380332547f8d3
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2024-01-25 16:44:06 +0100

    connect generated package
    Signed-off-by: chenyihao9 <286884497@qq.com>

--- tester/harmony/entry/src/main/cpp/PackageProvider.cpp
+++ tester/harmony/entry/src/main/cpp/PackageProvider.cpp
@@ -1,8 +1,9 @@
 #include "RNOH/PackageProvider.h"
 #include "GestureHandlerPackage.h"
+#include "generated/RNOHGeneratedPackage.h"
 
 using namespace rnoh;
 
 std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
-    return {std::make_shared<GestureHandlerPackage>(ctx)};
+    return {std::make_shared<RNOHGeneratedPackage>(ctx), std::make_shared<GestureHandlerPackage>(ctx)};
 }
