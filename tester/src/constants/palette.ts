b49693a71511a02a9a0947746a8c725df49e0591
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2024-01-26 15:24:40 +0100

    move new api tests to separate file
    Signed-off-by: chenyihao9 <286884497@qq.com>

---
+++ tester/src/constants/palette.ts
@@ -0,0 +1,4 @@
+export const PALETTE = {
+  LIGHT_GREEN: '#56a23e',
+  DARK_BLUE: '#2c647f',
+};
---
+++ tester/src/tests/NewApiTest.tsx
@@ -0,0 +1,176 @@
+import {TestCase, TestSuite} from '@rnoh/testerino';
+import React from 'react';
+import {useState} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import {
+  Gesture,
+  GestureDetector,
+  GestureType,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from 'react-native-gesture-handler';
+import {PALETTE} from '../constants';
+
+export function NewApiTest() {
+  return (
+    <TestSuite name="new API">
+      <TestCase
+        itShould="toggle color on tap"
+        initialState={false}
+        arrange={({setState}) => {
+          return (
+            <Example
+              label="PRESS ME"
+              createGesture={setBackgroundColor => {
+                return Gesture.Tap().onStart(() => {
+                  setState(true);
+                  setBackgroundColor(prev =>
+                    prev === PALETTE.DARK_BLUE
+                      ? PALETTE.LIGHT_GREEN
+                      : PALETTE.DARK_BLUE,
+                  );
+                });
+              }}
+            />
+          );
+        }}
+        assert={({expect, state}) => {
+          expect(state).to.be.true;
+        }}
+      />
+
+      <TestCase
+        itShould="change color to green when panning"
+        initialState={false}
+        arrange={({setState}) => {
+          return (
+            <Example
+              label="PAN ME"
+              createGesture={setBackgroundColor => {
+                return Gesture.Pan()
+                  .onStart(() => {
+                    setBackgroundColor(PALETTE.LIGHT_GREEN);
+                    setState(true);
+                  })
+                  .onEnd(() => {
+                    setBackgroundColor(PALETTE.DARK_BLUE);
+                  });
+              }}
+            />
+          );
+        }}
+        assert={({expect, state}) => {
+          expect(state).to.be.true;
+        }}
+      />
+
+      <TestCase
+        itShould="support TouchableOpacity"
+        initialState={false}
+        arrange={({setState}) => {
+          return (
+            <View style={styles.testCaseContainer}>
+              <TouchableOpacity
+                style={{
+                  width: 128,
+                  height: 128,
+                  backgroundColor: PALETTE.DARK_BLUE,
+                  justifyContent: 'center',
+                }}
+                onPress={() => {
+                  setState(true);
+                }}>
+                <Text
+                  style={{
+                    textAlign: 'center',
+                    fontSize: 12,
+                    color: 'white',
+                  }}>
+                  PRESS ME
+                </Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+        assert={({expect, state}) => {
+          expect(state).to.be.true;
+        }}
+      />
+
+      <TestCase
+        itShould="support TouchableWithoutFeedback"
+        initialState={false}
+        arrange={({setState}) => {
+          return (
+            <View style={styles.testCaseContainer}>
+              <TouchableWithoutFeedback
+                style={{
+                  width: 128,
+                  height: 128,
+                  backgroundColor: PALETTE.DARK_BLUE,
+                  justifyContent: 'center',
+                }}
+                onPress={() => {
+                  setState(true);
+                }}>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: 'white',
+                    textAlign: 'center',
+                  }}>
+                  PRESS ME
+                </Text>
+              </TouchableWithoutFeedback>
+            </View>
+          );
+        }}
+        assert={({expect, state}) => {
+          expect(state).to.be.true;
+        }}
+      />
+    </TestSuite>
+  );
+}
+
+function Example(props: {
+  label: string;
+  createGesture: (
+    setColor: React.Dispatch<React.SetStateAction<string>>,
+  ) => GestureType;
+  rightHitSlop?: number;
+}) {
+  const [backgroundColor, setBackgroundColor] = useState(PALETTE.DARK_BLUE);
+
+  const gesture = React.useMemo(() => {
+    return props.createGesture(setBackgroundColor);
+  }, []);
+
+  return (
+    <View style={styles.testCaseContainer}>
+      <GestureDetector gesture={gesture}>
+        <View
+          style={{
+            width: 128,
+            height: 128,
+            alignSelf: 'center',
+            backgroundColor,
+            justifyContent: 'center',
+          }}>
+          <Text style={{color: 'white', fontSize: 12, textAlign: 'center'}}>
+            {props.label}
+          </Text>
+        </View>
+      </GestureDetector>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  testCaseContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+});
