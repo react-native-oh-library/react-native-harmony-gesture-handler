b356ac877ebe196a28ca63de5b6f6b337536f0be
Zofia Glapa <zofia.glapa@swmansion.com>
2024-02-21 17:24:28 +0100

    Release react-native-harmony-gesture-handler@2.12.7
    This MR includes changes needed for release. It updates the RNOH dependency to v0.72.16 and does some changes connected to the update
    
    * adds `redirectInternalImports: true` flag to the package to allow importing `react-native-harmony-gesture-handler` files in `react-native-gesture-handler`. This triggers following changes:
      * `RNGestureHandlerModule` is now exported from its `.tsx` file using default export to match the original file
      * most of the files exported by this package are deleted as they are no longer needed - these were exact copies of `react-native-gesture-handler` sources which were forked to allow importing custom implementations of `RNGestureHandlerModule`, `createHandler`, `GestureHandlerRootView` and files included in `src/specs`. With internal imports redirection these can be imported straight from the original files.
    * adds passing context to `RNOHErrorDialog` in tester's `Index.ets`
    
    See merge request rnoh/react-native-harmony-gesture-handler!8

--- react-native-harmony-gesture-handler/src/components/GestureHandlerButton.tsx
+++
@@ -1,5 +0,0 @@
-import { HostComponent } from 'react-native';
-import { RawButtonProps } from 'react-native-gesture-handler/src/components/GestureButtons';
-import RNGestureHandlerButton from '../specs/RNGestureHandlerButtonNativeComponent';
-
-export default RNGestureHandlerButton as HostComponent<RawButtonProps>;
