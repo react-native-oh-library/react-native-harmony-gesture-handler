2d9abe75b3bffdad31a297a883a9591b2bc65236
Zofia Glapa <zofia.glapa@swmansion.com>
2024-02-21 17:34:31 +0100

    Revert "Merge branch '@zofia.glapa/release-gesture-handler' into 'main'"
    This reverts merge request !8.
    
    See merge request rnoh/react-native-harmony-gesture-handler!9

--- tester/App.tsx
+++ tester/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, StatusBar, View} from 'react-native';
+import {SafeAreaView, ScrollView, Text} from 'react-native';
 import {Tester} from '@rnoh/testerino';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {NewApiTest, OldApiTest} from './src';
 
 function App({}): JSX.Element | null {
   return (
-    <View style={{flex: 1}}>
-      <StatusBar />
-      <SafeAreaView style={{backgroundColor: '#222', flex: 1}}>
-        <GestureHandlerRootView style={{flex: 1}}>
-          <Tester style={{flex: 1}}>
-            <ScrollView style={{width: '100%', flex: 1}}>
-              <NewApiTest />
-              <OldApiTest />
-            </ScrollView>
-          </Tester>
-        </GestureHandlerRootView>
-      </SafeAreaView>
-    </View>
+    <SafeAreaView style={{backgroundColor: '#222', flex: 1}}>
+      <GestureHandlerRootView style={{flex: 1}}>
+        <Tester style={{flex: 1}}>
+          <ScrollView style={{width: '100%', flex: 1}}>
+            <NewApiTest />
+            <OldApiTest />
+          </ScrollView>
+        </Tester>
+      </GestureHandlerRootView>
+    </SafeAreaView>
   );
 }
 
