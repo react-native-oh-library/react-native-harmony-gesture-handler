d615c327329fc9ec7c125a1c42dbad62b74eebb6
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2024-04-23 17:02:57 +0200

    feat: reexport utility functions
    Closes https://gl.swmansion.com/rnoh/react-native-harmony-gesture-handler/-/issues/45
    
    See merge request rnoh/react-native-harmony-gesture-handler!19

--- tester/App.tsx
+++ tester/App.tsx
@@ -1,27 +1,54 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useState} from 'react';
 
-import {SafeAreaView, ScrollView, StatusBar, View} from 'react-native';
+import {Button, SafeAreaView, ScrollView, StatusBar, View} from 'react-native';
 import {Tester} from '@rnoh/testerino';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {NewApiTest, OldApiTest} from './src';
+import {
+  GestureHandlerRootView,
+  gestureHandlerRootHOC,
+} from 'react-native-gesture-handler';
+import {NewApiTest, OldApiTest, SharedAPITest} from './src';
+
+type RootViewMode = 'Component' | 'HOC';
 
 function App({}): JSX.Element | null {
+  const [rootViewMode, setRootViewMode] = useState<RootViewMode>('Component');
+
   return (
     <View style={{flex: 1}}>
       <StatusBar />
       <SafeAreaView style={{backgroundColor: '#222', flex: 1}}>
-        <GestureHandlerRootView style={{flex: 1}}>
-          <Tester style={{flex: 1}}>
-            <ScrollView style={{width: '100%', flex: 1}}>
-              <NewApiTest />
-              <OldApiTest />
-            </ScrollView>
-          </Tester>
-        </GestureHandlerRootView>
+        <Button
+          title={`toggle rootViewMode (current: ${rootViewMode})`}
+          onPress={() => {
+            setRootViewMode(prev =>
+              prev === 'Component' ? 'HOC' : 'Component',
+            );
+          }}
+        />
+        {rootViewMode === 'Component' && (
+          <GestureHandlerRootView style={{flex: 1}}>
+            <Tests />
+          </GestureHandlerRootView>
+        )}
+        {rootViewMode === 'HOC' && <WrappedTests />}
       </SafeAreaView>
     </View>
   );
 }
+
+function Tests() {
+  return (
+    <Tester style={{flex: 1}}>
+      <ScrollView style={{width: '100%', flex: 1}}>
+        <SharedAPITest />
+        <NewApiTest />
+        <OldApiTest />
+      </ScrollView>
+    </Tester>
+  );
+}
+
+const WrappedTests = gestureHandlerRootHOC(Tests);
 
 export default App;
