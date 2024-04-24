9b284f9a6e294a222fd9bc1ec4b06c907d074494
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2024-04-24 18:07:01 +0200

    feat: PureNativeButton
    Closes https://gl.swmansion.com/rnoh/react-native-harmony-gesture-handler/-/issues/50
    
    See merge request rnoh/react-native-harmony-gesture-handler!25
    
    Signed-off-by: chenyihao9 <286884497@qq.com>

--- react-native-harmony-gesture-handler/src/index.ts
+++ react-native-harmony-gesture-handler/src/index.ts
@@ -10,52 +10,52 @@ import { initialize } from 'react-native-gesture-handler/src/init';
 export { State } from 'react-native-gesture-handler/src/State';
 export { default as gestureHandlerRootHOC } from 'react-native-gesture-handler/src/components/gestureHandlerRootHOC';
 export { default as GestureHandlerRootView } from './components/GestureHandlerRootView';
-// export type {
-//   // event types
-//   GestureEvent,
-//   HandlerStateChangeEvent,
-//   // event payloads types
-//   GestureEventPayload,
-//   HandlerStateChangeEventPayload,
-//   // pointer events
-//   GestureTouchEvent,
-//   TouchData,
-//   // new api event types
-//   GestureUpdateEvent,
-//   GestureStateChangeEvent,
-// } from './handlers/gestureHandlerCommon';
+export type {
+  // event types
+  GestureEvent,
+  HandlerStateChangeEvent,
+  // event payloads types
+  GestureEventPayload,
+  HandlerStateChangeEventPayload,
+  // pointer events
+  GestureTouchEvent,
+  TouchData,
+  // new api event types
+  GestureUpdateEvent,
+  GestureStateChangeEvent,
+} from 'react-native-gesture-handler/src/handlers/gestureHandlerCommon';
 export type { GestureType } from 'react-native-gesture-handler/src/handlers/gestures/gesture';
-// export type {
-//   TapGestureHandlerEventPayload,
-//   TapGestureHandlerProps,
-// } from './handlers/TapGestureHandler';
-// export type {
-//   ForceTouchGestureHandlerEventPayload,
-//   ForceTouchGestureHandlerProps,
-// } from './handlers/ForceTouchGestureHandler';
-// export type { ForceTouchGestureChangeEventPayload } from './handlers/gestures/forceTouchGesture';
-// export type {
-//   LongPressGestureHandlerEventPayload,
-//   LongPressGestureHandlerProps,
-// } from './handlers/LongPressGestureHandler';
-// export type {
-//   PanGestureHandlerEventPayload,
-//   PanGestureHandlerProps,
-// } from './handlers/PanGestureHandler';
-// export type { PanGestureChangeEventPayload } from './handlers/gestures/panGesture';
-// export type {
-//   PinchGestureHandlerEventPayload,
-//   PinchGestureHandlerProps,
-// } from './handlers/PinchGestureHandler';
-// export type { PinchGestureChangeEventPayload } from './handlers/gestures/pinchGesture';
-// export type {
-//   RotationGestureHandlerEventPayload,
-//   RotationGestureHandlerProps,
-// } from './handlers/RotationGestureHandler';
-// export type {
-//   FlingGestureHandlerEventPayload,
-//   FlingGestureHandlerProps,
-// } from './handlers/FlingGestureHandler';
+export type {
+  TapGestureHandlerEventPayload,
+  TapGestureHandlerProps,
+} from 'react-native-gesture-handler/src/handlers/TapGestureHandler';
+export type {
+  ForceTouchGestureHandlerEventPayload,
+  ForceTouchGestureHandlerProps,
+} from 'react-native-gesture-handler/src/handlers/ForceTouchGestureHandler';
+export type { ForceTouchGestureChangeEventPayload } from 'react-native-gesture-handler/src/handlers/gestures/forceTouchGesture';
+export type {
+  LongPressGestureHandlerEventPayload,
+  LongPressGestureHandlerProps,
+} from 'react-native-gesture-handler/src/handlers/LongPressGestureHandler';
+export type {
+  PanGestureHandlerEventPayload,
+  PanGestureHandlerProps,
+} from 'react-native-gesture-handler/src/handlers/PanGestureHandler';
+export type { PanGestureChangeEventPayload } from 'react-native-gesture-handler/src/handlers/gestures/panGesture';
+export type {
+  PinchGestureHandlerEventPayload,
+  PinchGestureHandlerProps,
+} from 'react-native-gesture-handler/src/handlers/PinchGestureHandler';
+export type { PinchGestureChangeEventPayload } from 'react-native-gesture-handler/src/handlers/gestures/pinchGesture';
+export type {
+  RotationGestureHandlerEventPayload,
+  RotationGestureHandlerProps,
+} from 'react-native-gesture-handler/src/handlers/RotationGestureHandler';
+export type {
+  FlingGestureHandlerEventPayload,
+  FlingGestureHandlerProps,
+} from 'react-native-gesture-handler/src/handlers/FlingGestureHandler';
 export { TapGestureHandler } from 'react-native-gesture-handler/src/handlers/TapGestureHandler';
 // export { ForceTouchGestureHandler } from './handlers/ForceTouchGestureHandler';
 // export { LongPressGestureHandler } from './handlers/LongPressGestureHandler';
@@ -64,42 +64,42 @@ export { PanGestureHandler } from 'react-native-gesture-handler/src/handlers/Pan
 // export { RotationGestureHandler } from './handlers/RotationGestureHandler';
 // export { FlingGestureHandler } from './handlers/FlingGestureHandler';
 export { default as createNativeWrapper } from 'react-native-gesture-handler/src/handlers/createNativeWrapper';
-// export type {
-//   NativeViewGestureHandlerPayload,
-//   NativeViewGestureHandlerProps,
-// } from './handlers/NativeViewGestureHandler';
+export type {
+  NativeViewGestureHandlerPayload,
+  NativeViewGestureHandlerProps,
+} from 'react-native-gesture-handler/src/handlers/NativeViewGestureHandler';
 export { GestureDetector } from 'react-native-gesture-handler/src/handlers/gestures/GestureDetector';
 export { GestureObjects as Gesture } from 'react-native-gesture-handler/src/handlers/gestures/gestureObjects';
-// export type { TapGestureType as TapGesture } from './handlers/gestures/tapGesture';
-// export type { PanGestureType as PanGesture } from './handlers/gestures/panGesture';
-// export type { FlingGestureType as FlingGesture } from './handlers/gestures/flingGesture';
-// export type { LongPressGestureType as LongPressGesture } from './handlers/gestures/longPressGesture';
-// export type { PinchGestureType as PinchGesture } from './handlers/gestures/pinchGesture';
-// export type { RotationGestureType as RotationGesture } from './handlers/gestures/rotationGesture';
-// export type { ForceTouchGestureType as ForceTouchGesture } from './handlers/gestures/forceTouchGesture';
-// export type { NativeGestureType as NativeGesture } from './handlers/gestures/nativeGesture';
-// export type { ManualGestureType as ManualGesture } from './handlers/gestures/manualGesture';
-// export type {
-//   ComposedGestureType as ComposedGesture,
-//   RaceGestureType as RaceGesture,
-//   SimultaneousGestureType as SimultaneousGesture,
-//   ExclusiveGestureType as ExclusiveGesture,
-// } from './handlers/gestures/gestureComposition';
-// export type { GestureStateManagerType as GestureStateManager } from './handlers/gestures/gestureStateManager';
+export type { TapGestureType as TapGesture } from 'react-native-gesture-handler/src/handlers/gestures/tapGesture';
+export type { PanGestureType as PanGesture } from 'react-native-gesture-handler/src/handlers/gestures/panGesture';
+export type { FlingGestureType as FlingGesture } from 'react-native-gesture-handler/src/handlers/gestures/flingGesture';
+export type { LongPressGestureType as LongPressGesture } from 'react-native-gesture-handler/src/handlers/gestures/longPressGesture';
+export type { PinchGestureType as PinchGesture } from 'react-native-gesture-handler/src/handlers/gestures/pinchGesture';
+export type { RotationGestureType as RotationGesture } from 'react-native-gesture-handler/src/handlers/gestures/rotationGesture';
+export type { ForceTouchGestureType as ForceTouchGesture } from 'react-native-gesture-handler/src/handlers/gestures/forceTouchGesture';
+export type { NativeGestureType as NativeGesture } from 'react-native-gesture-handler/src/handlers/gestures/nativeGesture';
+export type { ManualGestureType as ManualGesture } from 'react-native-gesture-handler/src/handlers/gestures/manualGesture';
+export type {
+  ComposedGestureType as ComposedGesture,
+  RaceGestureType as RaceGesture,
+  SimultaneousGestureType as SimultaneousGesture,
+  ExclusiveGestureType as ExclusiveGesture,
+} from 'react-native-gesture-handler/src/handlers/gestures/gestureComposition';
+export type { GestureStateManagerType as GestureStateManager } from 'react-native-gesture-handler/src/handlers/gestures/gestureStateManager';
 // export { NativeViewGestureHandler } from './handlers/NativeViewGestureHandler';
-// export type {
-//   RawButtonProps,
-//   BaseButtonProps,
-//   RectButtonProps,
-//   BorderlessButtonProps,
-// } from './components/GestureButtons';
-// export {
-//   RawButton,
-//   BaseButton,
-//   RectButton,
-//   BorderlessButton,
-//   PureNativeButton,
-// } from './components/GestureButtons';
+export type {
+  RawButtonProps,
+  BaseButtonProps,
+  RectButtonProps,
+  BorderlessButtonProps,
+} from 'react-native-gesture-handler/src/components/GestureButtons';
+export {
+  RawButton,
+  BaseButton,
+  RectButton,
+  BorderlessButton,
+  PureNativeButton,
+} from 'react-native-gesture-handler/src/components/GestureButtons';
 export {
   TouchableHighlight,
   TouchableNativeFeedback,
@@ -114,44 +114,44 @@ export {
   FlatList,
   RefreshControl,
 } from 'react-native-gesture-handler/src/components/GestureComponents';
-// export type {
-//   //events
-//   GestureHandlerGestureEvent,
-//   GestureHandlerStateChangeEvent,
-//   //event payloads
-//   GestureHandlerGestureEventNativeEvent,
-//   GestureHandlerStateChangeNativeEvent,
-//   NativeViewGestureHandlerGestureEvent,
-//   NativeViewGestureHandlerStateChangeEvent,
-//   TapGestureHandlerGestureEvent,
-//   TapGestureHandlerStateChangeEvent,
-//   ForceTouchGestureHandlerGestureEvent,
-//   ForceTouchGestureHandlerStateChangeEvent,
-//   LongPressGestureHandlerGestureEvent,
-//   LongPressGestureHandlerStateChangeEvent,
-//   PanGestureHandlerGestureEvent,
-//   PanGestureHandlerStateChangeEvent,
-//   PinchGestureHandlerGestureEvent,
-//   PinchGestureHandlerStateChangeEvent,
-//   RotationGestureHandlerGestureEvent,
-//   RotationGestureHandlerStateChangeEvent,
-//   FlingGestureHandlerGestureEvent,
-//   FlingGestureHandlerStateChangeEvent,
-//   // handlers props
-//   NativeViewGestureHandlerProperties,
-//   TapGestureHandlerProperties,
-//   LongPressGestureHandlerProperties,
-//   PanGestureHandlerProperties,
-//   PinchGestureHandlerProperties,
-//   RotationGestureHandlerProperties,
-//   FlingGestureHandlerProperties,
-//   ForceTouchGestureHandlerProperties,
-//   // buttons props
-//   RawButtonProperties,
-//   BaseButtonProperties,
-//   RectButtonProperties,
-//   BorderlessButtonProperties,
-// } from './handlers/gestureHandlerTypesCompat';
+export type {
+  //events
+  GestureHandlerGestureEvent,
+  GestureHandlerStateChangeEvent,
+  //event payloads
+  GestureHandlerGestureEventNativeEvent,
+  GestureHandlerStateChangeNativeEvent,
+  NativeViewGestureHandlerGestureEvent,
+  NativeViewGestureHandlerStateChangeEvent,
+  TapGestureHandlerGestureEvent,
+  TapGestureHandlerStateChangeEvent,
+  ForceTouchGestureHandlerGestureEvent,
+  ForceTouchGestureHandlerStateChangeEvent,
+  LongPressGestureHandlerGestureEvent,
+  LongPressGestureHandlerStateChangeEvent,
+  PanGestureHandlerGestureEvent,
+  PanGestureHandlerStateChangeEvent,
+  PinchGestureHandlerGestureEvent,
+  PinchGestureHandlerStateChangeEvent,
+  RotationGestureHandlerGestureEvent,
+  RotationGestureHandlerStateChangeEvent,
+  FlingGestureHandlerGestureEvent,
+  FlingGestureHandlerStateChangeEvent,
+  // handlers props
+  NativeViewGestureHandlerProperties,
+  TapGestureHandlerProperties,
+  LongPressGestureHandlerProperties,
+  PanGestureHandlerProperties,
+  PinchGestureHandlerProperties,
+  RotationGestureHandlerProperties,
+  FlingGestureHandlerProperties,
+  ForceTouchGestureHandlerProperties,
+  // buttons props
+  RawButtonProperties,
+  BaseButtonProperties,
+  RectButtonProperties,
+  BorderlessButtonProperties,
+} from 'react-native-gesture-handler/src/handlers/gestureHandlerTypesCompat';
 
 export { default as Swipeable } from 'react-native-gesture-handler/src/components/Swipeable';
 export type {
