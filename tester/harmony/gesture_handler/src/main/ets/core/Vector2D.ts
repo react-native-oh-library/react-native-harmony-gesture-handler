9ab27a3a270fd94b5bd4e853b1eb54bc92bd35b1
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2024-04-30 13:34:11 +0200

    feat: Gesture.Fling; fix: event timestamp
    Closes https://gl.swmansion.com/rnoh/react-native-harmony-gesture-handler/-/issues/52
    
    See merge request rnoh/react-native-harmony-gesture-handler!31

--- tester/harmony/gesture_handler/src/main/ets/core/Vector2D.ts
+++ tester/harmony/gesture_handler/src/main/ets/core/Vector2D.ts
@@ -1,4 +1,26 @@
+import { Directions, DiagonalDirections } from "./IncomingEvent"
+
+type Cosine = number
+
 export class Vector2D {
+  static fromDirection(direction: Directions | DiagonalDirections): Vector2D {
+    const DirectionToVectorMappings = new Map<
+      Directions | DiagonalDirections,
+      Vector2D
+    >([
+      [Directions.LEFT, new Vector2D({ x: -1, y: 0 })],
+      [Directions.RIGHT, new Vector2D({ x: 1, y: 0 })],
+      [Directions.UP, new Vector2D({ x: 0, y: -1 })],
+      [Directions.DOWN, new Vector2D({ x: 0, y: 1 })],
+      [DiagonalDirections.UP_RIGHT, new Vector2D({ x: 1, y: -1 })],
+      [DiagonalDirections.DOWN_RIGHT, new Vector2D({ x: 1, y: 1 })],
+      [DiagonalDirections.UP_LEFT, new Vector2D({ x: -1, y: -1 })],
+      [DiagonalDirections.DOWN_LEFT, new Vector2D({ x: -1, y: 1 })],
+    ]);
+    return DirectionToVectorMappings.get(direction)!;
+  }
+
+
   constructor(
     private val: {
       x: number;
@@ -32,5 +54,26 @@ export class Vector2D {
     this.val.x += vec.x;
     this.val.y += vec.y;
     return this;
+  }
+
+  public createUnitVector(): Vector2D {
+    const magnitude = this.magnitude
+    if (magnitude === 0) {
+      return new Vector2D({ x: 0, y: 0 });
+    }
+    return new Vector2D({
+      x: this.val.x / magnitude,
+      y: this.val.y / magnitude
+    });
+  }
+
+  public computeCosine(other: Vector2D): Cosine {
+    const thisUnit = this.createUnitVector();
+    const otherUnit = other.createUnitVector();
+    return thisUnit.val.x * otherUnit.val.x + thisUnit.val.y * otherUnit.val.y;
+  }
+
+  public get magnitude() {
+    return Math.hypot(this.x, this.y);
   }
 }
--- tester/harmony/gesture_handler/src/main/ets/rnoh/RNGHRootTouchHandlerCAPI.ts
+++ tester/harmony/gesture_handler/src/main/ets/rnoh/RNGHRootTouchHandlerCAPI.ts
@@ -1,71 +1,83 @@
-import { RNGHRootTouchHandlerArkTS } from "./RNGHRootTouchHandlerArkTS"
-import { TouchEvent as TouchEventArkTS, TouchType, TouchObject } from "./types"
-import { RNGHLogger } from "../core"
+import { RNGHRootTouchHandlerArkTS } from './RNGHRootTouchHandlerArkTS';
+import { TouchEvent as TouchEventArkTS, TouchType, TouchObject } from './types';
+import { RNGHLogger } from '../core';
 
 type RawTouchPoint = {
-  pointerId: number
-  windowX: number
-  windowY: number
-
-}
+  pointerId: number;
+  windowX: number;
+  windowY: number;
+};
 
 export type RawTouchEvent = {
-  action: number,
-  actionTouch: RawTouchPoint,
-  touchPoints: RawTouchPoint[],
-  sourceType: number,
-  timestamp: number
-}
+  action: number;
+  actionTouch: RawTouchPoint;
+  touchPoints: RawTouchPoint[];
+  sourceType: number;
+  timestamp: number;
+};
 
 class TouchEvent {
   constructor(private raw: RawTouchEvent) {
   }
 
   asTouchEventArkTS(): TouchEventArkTS {
-    const touchType = this.touchTypeFromAction(this.raw.action)
+    const touchType = this.touchTypeFromAction(this.raw.action);
     return {
       type: this.touchTypeFromAction(this.raw.action),
-      touches: this.raw.touchPoints.map(tp => this.touchObjectFromTouchPoint(tp, touchType)),
-      changedTouches:[this.touchObjectFromTouchPoint(this.raw.actionTouch, touchType)],
-      timestamp: this.raw.timestamp
-    }
+      touches: this.raw.touchPoints.map(tp =>
+      this.touchObjectFromTouchPoint(tp, touchType),
+      ),
+      changedTouches: [
+        this.touchObjectFromTouchPoint(this.raw.actionTouch, touchType),
+      ],
+      timestamp: this.raw.timestamp / Math.pow(10, 6),
+    };
   }
 
   private touchTypeFromAction(action: number): TouchType {
     switch (action) {
       case 1:
-        return TouchType.Down
+        return TouchType.Down;
       case 2:
-        return TouchType.Move
+        return TouchType.Move;
       case 3:
-        return TouchType.Up
+        return TouchType.Up;
       default:
-        return TouchType.Cancel
+        return TouchType.Cancel;
     }
   }
 
-  private touchObjectFromTouchPoint(touchPoint: RawTouchPoint, touchType: TouchType): TouchObject {
+  private touchObjectFromTouchPoint(
+    touchPoint: RawTouchPoint,
+    touchType: TouchType,
+  ): TouchObject {
     return {
       id: touchPoint.pointerId,
       windowX: touchPoint.windowX,
       windowY: touchPoint.windowY,
       x: touchPoint.windowX,
       y: touchPoint.windowY,
-      type: touchType
-    }
+      type: touchType,
+    };
   }
 }
 
 export class RNGHRootTouchHandlerCAPI {
-  private logger: RNGHLogger
+  private logger: RNGHLogger;
 
-  constructor(logger: RNGHLogger, private touchHandlerArkTS: RNGHRootTouchHandlerArkTS) {
-    this.logger = logger.cloneWithPrefix("RNGHRootTouchHandlerCAPI")
+  constructor(
+    logger: RNGHLogger,
+    private touchHandlerArkTS: RNGHRootTouchHandlerArkTS,
+  ) {
+    this.logger = logger.cloneWithPrefix('RNGHRootTouchHandlerCAPI');
   }
 
   handleTouch(rawTouchEvent: RawTouchEvent) {
-    this.logger.cloneWithPrefix("handleTouch").debug(JSON.stringify(rawTouchEvent))
-    this.touchHandlerArkTS.handleTouch(new TouchEvent(rawTouchEvent).asTouchEventArkTS())
+    this.logger
+      .cloneWithPrefix('handleTouch')
+      .debug(JSON.stringify(rawTouchEvent));
+    this.touchHandlerArkTS.handleTouch(
+      new TouchEvent(rawTouchEvent).asTouchEventArkTS(),
+    );
   }
 }
-
