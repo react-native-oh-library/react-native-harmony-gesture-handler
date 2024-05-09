30b12626be756a78932b376e84fba4e73d634639
haoxuchao <hWX1289045@china.huawei.com>
2024-05-09 10:22:31 +0800

    release:react-native-oh-tpl-react-native-gesture-handler@2.14.1
    Signed-off-by: haoxuchao <hWX1289045@china.huawei.com>

--- react-native-gesture-handler/harmony/gesture_handler/src/main/cpp/GestureHandlerPackage.h
+++ react-native-gesture-handler/harmony/gesture_handler/src/main/cpp/GestureHandlerPackage.h
@@ -6,12 +6,6 @@ namespace rnoh {
     public:
         GestureHandlerPackage(Package::Context ctx) : Package(ctx) {}
 
-        std::unique_ptr<TurboModuleFactoryDelegate> createTurboModuleFactoryDelegate() override;
-
-        std::vector<facebook::react::ComponentDescriptorProvider> createComponentDescriptorProviders() override;
-
-        ComponentJSIBinderByString createComponentJSIBinderByName() override;
-
         EventEmitRequestHandlers createEventEmitRequestHandlers();
 
         ComponentInstanceFactoryDelegate::Shared createComponentInstanceFactoryDelegate();
