16dba24464a77643c9cc40066d7dced4f7acd906
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2024-05-10 18:50:03 +0800

    optimization: detect touchable views at the cpp side
    Closes https://gl.swmansion.com/rnoh/react-native-harmony-gesture-handler/-/issues/65
    
    See merge request rnoh/react-native-harmony-gesture-handler!37
    
    (cherry picked from commit a4c9559ec25a23102bedf721edfc3ca1db6dcaa5)

--- tester/harmony/gesture_handler/src/main/ets/rnoh/ViewRegistry.ts
+++ tester/harmony/gesture_handler/src/main/ets/rnoh/ViewRegistry.ts
@@ -1,33 +1,45 @@
-import {
-  DescriptorRegistry,
-  ComponentManagerRegistry,
-  Tag,
-} from '@rnoh/react-native-openharmony/ts';
-import {View} from './View';
+import { DescriptorRegistry, Tag, } from '@rnoh/react-native-openharmony/ts';
+import { ViewArkTS } from './View';
+import { View } from '../core';
+import type { ViewFinder } from "./RNGHRootTouchHandlerArkTS"
 
-export class ViewRegistry {
+export interface ViewRegistry extends ViewFinder {
+  getViewByTag(viewTag: Tag)
+  save(view: View)
+}
+
+export class ViewRegistryArkTS implements ViewRegistry {
   constructor(
     private descriptorRegistry: DescriptorRegistry,
-    private componentManagerRegistry: ComponentManagerRegistry,
-  ) {}
+  ) {
+  }
 
-  public getViewByTag(viewTag: Tag) {
+  getViewByTag(viewTag: Tag) {
     return this.createView(viewTag);
   }
 
-  private createView(tag: Tag): View {
-    return new View(this.descriptorRegistry, tag);
+  save(view: View) {
+    /**
+     * Currently, a new View object is created when getViewByTag is called. That was the approach in initial "quick"
+     * implementation of this library. The save was introduced later, for the needs of implementation using C-API architecture.
+     * C-API architecture is going to replace ArkTS anyway, so there's no point of doing this properly since this class
+     * will be removed in the future.
+     */
   }
 
-  public getTouchableViewsAt(
+  private createView(tag: Tag): ViewArkTS {
+    return new ViewArkTS(this.descriptorRegistry, tag);
+  }
+
+  getTouchableViewsAt(
     pos: {
       x: number;
       y: number;
     },
     rootTag: Tag,
-  ): View[] {
+  ): ViewArkTS[] {
     const rootView = this.createView(rootTag);
-    const results: View[] = [];
+    const results: ViewArkTS[] = [];
     for (const view of this.getTouchableViewsAtPosInView(pos, rootView)) {
       results.push(view);
     }
@@ -39,16 +51,45 @@ export class ViewRegistry {
       x: number;
       y: number;
     },
-    view: View,
+    view: ViewArkTS,
   ) {
     if (!view.isPositionInBounds(pos)) return [];
-    const results: View[] = [];
+    const results: ViewArkTS[] = [];
     results.push(view);
-    for (const child of view.getChildren()) {
+    for (const child of this.getChildrenOf(view.getTag())) {
       for (const result of this.getTouchableViewsAtPosInView(pos, child)) {
         results.push(result);
       }
     }
     return results;
+  }
+
+  private getChildrenOf(viewTag: Tag): ViewArkTS[] {
+    return this.descriptorRegistry.findDescriptorWrapperByTag(viewTag).childrenTags.map((tag) => new ViewArkTS(this.descriptorRegistry, tag))
+  }
+}
+
+
+export class ViewRegistryCAPI implements ViewRegistry {
+  private viewByTag = new Map<Tag, View>()
+
+  save(view: View) {
+    this.viewByTag.set(view.getTag(), view)
+  }
+
+  getViewByTag(viewTag: Tag) {
+    return this.viewByTag.get(viewTag);
+  }
+
+  getTouchableViewsAt(
+    pos: {
+      x: number;
+      y: number;
+    },
+    rootTag: Tag,
+  ): ViewArkTS[] {
+    // Finding views is handled on CPP side and provided with the touch event.
+    // This method can be removed from ViewRegistry interface once support for ArkTS architecture is removed.
+    return []
   }
 }
