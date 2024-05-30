05613477ba1fa955f9441760f70558fdb9506475
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2024-05-30 18:57:01 +0800

    fix: panning in nested GestureHandlerRootViews
    Closes https://gl.swmansion.com/rnoh/react-native-harmony-gesture-handler/-/issues/69
    
    See merge request rnoh/react-native-harmony-gesture-handler!46
    
    (cherry picked from commit d28f3e80fdceb0980696a5c72a968f95a7039b76)
    Signed-off-by: chenyihao9 <286884497@qq.com>

--- tester/index.js
+++ tester/index.js
@@ -1,5 +1,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Demo from './Demo';
 
 AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => Demo);
