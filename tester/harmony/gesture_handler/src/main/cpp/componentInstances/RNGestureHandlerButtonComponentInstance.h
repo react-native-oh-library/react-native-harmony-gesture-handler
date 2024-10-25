d54ac1715d084316fc931fa19236657d722b82b9
Patryk Klatka <patryk.klatka@swmansion.com>
2024-10-25 20:00:02 +0800

    refactor: move implementation to .cpp files
    ## Summary
    
    This MR moves implementation to `.cpp` files for `*ComponentInstance.h` to improve code readability and bumps CLI version.
    
    ## Issue links
    Blocked by: https://gl.swmansion.com/rnoh/react-native-harmony/-/merge_requests/1604
    
    See merge request rnoh/react-native-harmony-gesture-handler!76
    
    (cherry picked from commit 9e37bc8e2fd1f729d7e79670781d9376a2762fcb)

--- tester/harmony/gesture_handler/src/main/cpp/componentInstances/RNGestureHandlerButtonComponentInstance.h
+++ tester/harmony/gesture_handler/src/main/cpp/componentInstances/RNGestureHandlerButtonComponentInstance.h
@@ -6,22 +6,14 @@
 namespace rnoh {
 class RNGestureHandlerButtonComponentInstance
     : public CppComponentInstance<facebook::react::RNGestureHandlerButtonShadowNode> {
+public:
+    RNGestureHandlerButtonComponentInstance(Context context);
+
+    StackNode &getLocalRootArkUINode() override;
+    void onChildInserted(ComponentInstance::Shared const &childComponentInstance, std::size_t index) override;
+    void onChildRemoved(ComponentInstance::Shared const &childComponentInstance) override;
+
 private:
     StackNode m_stackNode;
-
-public:
-    RNGestureHandlerButtonComponentInstance(Context context) : CppComponentInstance(std::move(context)){};
-
-    StackNode &getLocalRootArkUINode() override { return m_stackNode; };
-
-    void onChildInserted(ComponentInstance::Shared const &childComponentInstance, std::size_t index) override {
-        CppComponentInstance::onChildInserted(childComponentInstance, index);
-        m_stackNode.insertChild(childComponentInstance->getLocalRootArkUINode(), index);
-    };
-
-    void onChildRemoved(ComponentInstance::Shared const &childComponentInstance) override {
-        CppComponentInstance::onChildRemoved(childComponentInstance);
-        m_stackNode.removeChild(childComponentInstance->getLocalRootArkUINode());
-    };
 };
 } // namespace rnoh
