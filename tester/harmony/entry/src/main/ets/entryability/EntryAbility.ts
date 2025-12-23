e5d1b21505df14eaeca0312e2af7952a3c8f37ad
wangyue <wangyue334@h-partners.com>
2025-12-23 18:18:23 +0800

    fix: 冲突解决
    Signed-off-by: wangyue <wangyue334@h-partners.com>

---
+++ tester/harmony/entry/src/main/ets/entryability/EntryAbility.ts
@@ -0,0 +1,12 @@
+import { RNAbility } from 'rnoh/ts';
+
+export default class EntryAbility extends RNAbility {
+    getPagePath() {
+        return "pages/Index"
+    }
+
+    getBundleURL() {
+        return "http://localhost:8081/index.bundle?platform=harmony&dev=false&minify=false"
+    }
+};
+
